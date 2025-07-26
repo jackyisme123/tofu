9f6d570ef7ef18ba8b6fa17b2c1f6903274f3934
Yuan Cui <jackyisme123@gmail.com>
2025-07-26 19:17:12 +1200

    change tf file fmt

--- labs/development/main.tf
+++ labs/development/main.tf
@@ -18,26 +18,26 @@ resource "aws_vpc" "production" {
   tags = {
     Name        = "${var.environment}-vpc"
     Environment = var.environment
-    Project = var.project_name
+    Project     = var.project_name
     ManagedBy   = "terraform"
     Region      = data.aws_region.current.name
-    AccountID = data.aws_caller_identity.current.account_id # <-- add value here
+    AccountID   = data.aws_caller_identity.current.account_id # <-- add value here
   }
 }
 
 resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.production.id
-  cidr_block              = var.subnet_cidr # <-- update value here
+  cidr_block              = var.subnet_cidr                                # <-- update value here
   availability_zone       = data.aws_availability_zones.available.names[0] # <-- update value here
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.environment}-private-subnet" # <-- update value here
-    Environment = var.environment # <-- update value here
-    Project = var.project_name # <-- update value here
-    ManagedBy = "terraform"
-    Region = data.aws_region.current.name # <-- update value here
-    AZ = data.aws_availability_zones.available.names[0] # <-- update value here
+    Name        = "${var.environment}-private-subnet" # <-- update value here
+    Environment = var.environment                     # <-- update value here
+    Project     = var.project_name                    # <-- update value here
+    ManagedBy   = "terraform"
+    Region      = data.aws_region.current.name                   # <-- update value here
+    AZ          = data.aws_availability_zones.available.names[0] # <-- update value here
   }
 }
 
@@ -45,10 +45,10 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.production.id
 
   tags = {
-    Name = "${var.environment}-route-table" # <-- update value here
-    Environment = var.environment # <-- update value here
-    Project = var.project_name # <-- update value here
-    ManagedBy = "terraform"
-    Region = data.aws_region.current.name # <-- update value here
+    Name        = "${var.environment}-route-table" # <-- update value here
+    Environment = var.environment                  # <-- update value here
+    Project     = var.project_name                 # <-- update value here
+    ManagedBy   = "terraform"
+    Region      = data.aws_region.current.name # <-- update value here
   }
 }
